d1b874582fd1a5d5331075f16cb0ee44fefdaa1d
ivan <ivan.karezin@garden-scout.ru>
2024-11-27 14:46:00 +0300

    create module

--- config/web.php
+++ config/web.php
@@ -42,14 +42,17 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+    ],
+    'modules' => [
+        'api' => [
+            'class' => 'app\modules\api\Module'
+        ]
     ],
     'params' => $params,
 ];
@@ -67,7 +70,7 @@ if (YII_ENV_DEV) {
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
         // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '172.18.0.1', '::1'],
     ];
 }
 
